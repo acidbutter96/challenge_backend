50404cc5357a7580ed5990a61a46b2058f4e1df0
acidbutter96 <marknotas@outlook.com>
2021-02-05 09:37:30 -0300

    Scrapper creating

--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import requests
 
 options = Options()
 options.page_load_strategy = 'normal'
@@ -13,4 +14,9 @@ driver = webdriver.Chrome(p+'\\chrome_driver\\chromedriver.exe',options=options)
 driver.get('https://www2.susep.gov.br/safe/menumercado/REP2/Produto.aspx/Consultar')
 
 driver.find_element_by_class_name('botaoPesquisar')
-driver.find_element(By.ID('numeroProcesso'))
+driver.find_element(By.ID,'numeroProcesso').send_keys("15414.004053/2012-57" + Keys.ENTER)
+
+dl = driver.find_element(By.CLASS_NAME,'linkDownloadRelatorio').get_attribute('onclick').split('=')[1].split("'")[1]
+
+r = requests.get('https://www2.susep.gov.br'+dl)
+open('test.pdf', 'wb').write(r.content)
